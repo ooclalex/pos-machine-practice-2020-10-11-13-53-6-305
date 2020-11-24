72c6919870867f830583a468167f29d30c9fea88
Alex Liu <LIUAL14@OOCL.COM>
2020-11-24 18:08:42 +0800

    added get quantities function and context map

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,44 @@
 package pos.machine;
 
-import java.util.List;
+import java.util.*;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
-        return null;
+        HashMap<String, Integer> hashMapItems = getQuantity(barcodes);
+        ArrayList listReceipt = getPrice(hashMapItems);
+        listReceipt = getSubtotal(listReceipt);
+
+        return generateReceipt(listReceipt);
     }
+
+    private String generateReceipt(ArrayList listReceipt) {
+    }
+
+    private ArrayList getSubtotal(ArrayList listReceipt) {
+    }
+
+    private ArrayList getPrice(HashMap<String, Integer> listReceipt) {
+        ArrayList listReceipts = new ArrayList();
+        for (Map.Entry<String,Integer> entry : listReceipt.entrySet()) {
+            String barcode = entry.getKey();
+            int quantity = entry.getValue();
+            String name = "";
+
+        }
+        return listReceipts;
+    }
+
+    private HashMap<String, Integer> getQuantity(List<String> barcodes) {
+        HashMap<String,Integer> hashMapItems = new HashMap<String,Integer>();
+        for (String barcode : barcodes) {
+            if (hashMapItems.containsKey(barcode)) {
+                hashMapItems.put(barcode, hashMapItems.get(barcode) + 1);
+            } else {
+                hashMapItems.put(barcode, 1);
+            }
+        }
+        return hashMapItems;
+    }
+
+
 }
